e106b1daf7bddd474efea9948cd49e3381e70d54
iqubb <62174635+iqubb@users.noreply.github.com>
2021-11-21 20:04:48 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,14 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    m[nums[0]] = 0
+    
+    for i := 1; i < len(nums); i++ {
+        if value, ok := m[target-nums[i]]; ok {
+            return []int {i, value}
+        } else {
+            m[nums[i]] = i
+        }
+    }
+    
+    return []int {0, 0}
+}
