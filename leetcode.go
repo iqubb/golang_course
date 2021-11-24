523d344e573cbb5ff9b47f09248f8e6f4aca5009
iqubb <62174635+iqubb@users.noreply.github.com>
2021-11-24 18:41:42 +0300

    Update and rename main.go to leetcode.go

--- leetcode.go
+++ leetcode.go
@@ -3,7 +3,6 @@
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
     m[nums[0]] = 0
-    
     for i := 1; i < len(nums); i++ {
         if value, ok := m[target-nums[i]]; ok {
             return []int {i, value}
@@ -11,6 +10,5 @@ func twoSum(nums []int, target int) []int {
             m[nums[i]] = i
         }
     }
-    
     return []int {0, 0}
 }
